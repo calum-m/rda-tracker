8df5bae07932ba3c07756ceb9263a6c9b85a077b
Calum Muir <calumamuir@outlook.com>
2025-06-07 12:27:29 +0000

    Update authConfig.js to dynamically set redirectUri

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,8 +1,16 @@
+const productionRedirectUri = "https://tracker.highlandgrouprda.org.uk";
+const localRedirectUri = "http://localhost:3000";
+
+// Dynamically set the redirectUri based on the hostname
+const currentRedirectUri = window.location.hostname === "localhost" 
+                            ? localRedirectUri 
+                            : productionRedirectUri;
+
 export const msalConfig = {
   auth: {
     clientId: "6f7c94c6-dc0d-44dd-9349-35b67d355ddc", // This should be your actual app registration's client ID
-    authority: "https://login.microsoftonline.com/9a55df51-bc84-4aad-8886-ff259fea7ca3", // 👈 Replace YOUR_TENANT_ID with your actual Tenant ID
-    redirectUri: "http://localhost:3000", // or your production URL
+    authority: "https://login.microsoftonline.com/9a55df51-bc84-4aad-8886-ff259fea7ca3", 
+    redirectUri: currentRedirectUri, 
   },
   cache: {
     cacheLocation: "sessionStorage", // or "localStorage"
