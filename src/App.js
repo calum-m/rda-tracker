a2fecfaf7209ce4190a61d6dce0d6135da55c2ce
Calum Muir <calumamuir@outlook.com>
2025-06-09 14:21:36 +0000

    Refactor: Remove debug wrapper from PrivacyPolicyPage render

--- src/App.js
+++ src/App.js
@@ -293,17 +293,7 @@ const AppWithEmotionCache = () => {
       <CacheProvider value={emotionCache}>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <div id="privacy-policy-container-for-debug" style={{ padding: '20px', backgroundColor: 'lightyellow', minHeight: '100vh' }}>
-            <Typography variant="h5" component="h1" gutterBottom>
-              Privacy Policy Page Container (Debug)
-            </Typography>
-            <Typography variant="body1" paragraph>
-              If you are seeing this yellow container, the application has correctly routed to the privacy policy path in this new tab.
-              The actual content of the Privacy Policy page should be rendered below this message.
-            </Typography>
-            <hr />
-            <PrivacyPolicyPage />
-          </div>
+          <PrivacyPolicyPage />
         </ThemeProvider>
       </CacheProvider>
     );
